60a390b0de23ea8293a5ced9a3c66f268cd7b2fe
donnate <cdonnat@uchicago.edu>
2022-09-29 17:42:26 -0700

    clean up algorithms

--- src/algorithm.py
+++ src/algorithm.py
@@ -25,11 +25,12 @@ def cov_scorer(self, X):
 
 
 def logit_scorer(self, X, y):
-    return -loss_logit(X,y, self.beta).value      #loglik
+    return -logit_loss(X,y, self.beta).value      #loglik
 
 
 class Estimator(BaseEstimator):
-    def __init__(self, l1 = 0, l2 = 0, D = 0):
+    def __init__(self, l1 = 0, l2 = 0, D = 0,
+                 familiy:str = 'normal'):
         self.l1 = l1
         self.l2 = l2
         self.D = D
@@ -40,181 +41,189 @@ class Estimator(BaseEstimator):
 
     def predict(self, X):
         check_is_fitted(self, "beta")
-        return X @ self.beta
+        if self.family == 'normal':
+            return X @ self.beta
+        elif self.family == 'poisson':
+            return np.exp(X @ self.beta)
+        elif self.family == 'binomial':
+            return np.round(np.exp(X @ self.beta)/(1 +np.exp(X @ self.beta)))
+        else:
+            print("Not implemented yet")
+            return
 
     def score(self, X, y):
         check_is_fitted(self, "beta")
-        return -cp.norm2(y - X @ self.beta).value**2 /X.shape[0]
+        if self.family == 'normal':
+            return -cp.norm2(y - X @ self.beta).value**2 /X.shape[0]
+        elif self.family == 'binomial':
+            return -logit_loss(X, y, self.beta).value
+        elif self.family == 'poisson':
+            return -poisson_loss(X,y, self.beta).value
 
     def l2_risk(self, beta_star):
         check_is_fitted(self, "beta")
-
         return cp.norm2(self.beta - beta_star).value
-
-
-class LogRegEstimator(BaseEstimator):
-    def __init__(self, l1:float =0, l2:float = 0, D = 0):
-        self.l1 = l1
-        self.l2 = l2
-        self.D = D
-    def predict(self, X):
-        check_is_fitted(self, "beta")
-        return np.round(np.exp(X @ self.beta) / (1 + np.exp(X @ self.beta)))
-
-    def score(self, X, y):
-        check_is_fitted(self, "beta")
-        return -loss_logit(X, y, self.beta).value
 
     def predict_accuracy(self, X, y):
         check_is_fitted(self, "beta")
-        return 1. - np.sum(np.abs(y - self.predict(X)))/len(y)
+        if self.family == 'binomial':
+            return 1. - np.sum(np.abs(y - self.predict(X)))/len(y)
+        else:
+            return None
 
-class LogRegGenEN(LogRegEstimator):
-    def __init__(self, l1=0, l2=0, D=None):
-        LogRegEstimator.__init__(self, l1=l1, l2=l2, D=D)
 
+class NaiveEstimator(Estimator):
+    def __init__(self, l1=0, l2=0, D=None, family='normal'):
+        Estimator.__init__(self, l1=l1, l2=l2, D=D,
+                                 family=family)
     def fit(self, X, y):
         n, p = X.shape
         beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(logit_loss(X, y, beta1) +
-                           EE_pen(beta1, self.l1, self.l2, self.D)))
-        prob1.solve()
-        self.beta = beta1.value
-        return self
-
-class LogFusedLasso(LogRegEstimator):
-    def __init__(self, l1=0, l2=0, D=None):
-            LogRegEstimator.__init__(self, l1=l1, l2=l2, D=D)
-
-    def fit(self, X, y):
-        n, p = X.shape
-        beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_logit(X, y, beta1) + FL_pen(beta1, self.l1, self.l2, self.D)))
-        prob1.solve()
-        self.beta = beta1.value
-        return self
-
-class LogSmoothLasso(LogRegEstimator):
-    def __init__(self, l1=0, l2=0, D=None):
-                LogRegEstimator.__init__(self, l1=l1, l2=l2, D=D)
-    def fit(self, X, y):
-        n, p = X.shape
-        beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_logit(X, y, beta1) +
-                           SL_pen(beta1, self.l1, self.l2, self.D)))
-        prob1.solve()
-        self.beta = beta1.value
-        return self
-
-class Log_OLR(LogEstimator):
-    def fit(self, X, y):
-        n, p = X.shape
-
-        beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_logit(X,y, beta1)))
-        prob1.solve()
-        self.beta = beta1.value
-        return self
-
-class PoissonEstimator(BaseEstimator):
-    def __init__(self, l1 = 0, l2 = 0, D = 0):
-        self.l1 = l1
-        self.l2 = l2
-        self.D = D
-    def predict(self, X):
-        check_is_fitted(self, "beta")
-        return np.exp(X @ self.beta)
-
-    def score(self, X, y):
-        check_is_fitted(self, "beta")
-        return -loss_poi(X,y, self.beta).value
-
-class Poisson_OUR(PoissonEstimator):
-    def fit(self, X, y):
-        n, p = X.shape
-
-        beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_poi(X, y, beta1) + EE_pen(beta1, self.l1, self.l2, self.D)))
-        prob1.solve()
-        self.beta = beta1.value
-        return self
-class Poisson_FL(PoissonEstimator):
-    def fit(self, X, y):
-        n, p = X.shape
-
-        beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_poi(X, y, beta1) + FL_pen(beta1, self.l1, self.l2, self.D)))
-        prob1.solve()
-        self.beta = beta1.value
-        return self
-class Poisson_SL(PoissonEstimator):
-    def fit(self, X, y):
-        n, p = X.shape
-
-        beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_poi(X, y, beta1) + SL_pen(beta1, self.l1, self.l2, self.D)))
+        if self.family == 'binomial':
+            prob1 = cp.Problem(cp.Minimize(logit_loss(X, y, beta1) +
+                               EE_pen(beta1, self.l1, self.l2, self.D)))
+        elif self.family == 'poisson':
+            prob1 = cp.Problem(cp.Minimize(poisson_loss(X, y, beta1)))
+        elif self.family == 'normal':
+            prob1 = cp.Problem(cp.Minimize(l2_loss(X, y, beta1) / n))
+        else:
+            raise ValueError('Exponential family not implemented yet')
         prob1.solve()
         self.beta = beta1.value
         return self
 
 
-
-class OLR(estimator):
+class LassoEstimator(Estimator):
+    def __init__(self, l1=0, family='binomial'):
+        Estimator.__init__(self, l1=l1, l2=None, D=None,
+                           family=family)
     def fit(self, X, y):
         n, p = X.shape
-
         beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_f2(X, y, beta1)/n)) ##ECOS always fail for OLR; might due to scaling
+        if self.family == 'binomial':
+            prob1 = cp.Problem(cp.Minimize(logit_loss(X, y, beta1) +
+                                           lasso_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        elif self.family == 'poisson':
+            prob1 = cp.Problem(cp.Minimize(poisson_loss(X, y, beta1) +
+                                           lasso_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        elif self.family == 'normal':
+            prob1 = cp.Problem(cp.Minimize(l2_loss(X, y, beta1) / n) +
+                                           lasso_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        else:
+            raise ValueError('Exponential family not implemented yet')
         prob1.solve()
         self.beta = beta1.value
         return self
 
-class LA(estimator):
+class FusedLassoEstimator(Estimator):
+    def __init__(self, l1=0, l2=0, D=None, family='normal'):
+            Estimator.__init__(self, l1=l1, l2=l2, D=D,
+                                     family=family)
     def fit(self, X, y):
         n, p = X.shape
-
         beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_f2(X, y, beta1) + L_pen(beta1, self.l1)))
+        if self.family == 'binomial':
+            prob1 = cp.Problem(cp.Minimize(logit_loss(X, y, beta1) +
+                                           fusedlasso_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        elif self.family == 'poisson':
+            prob1 = cp.Problem(cp.Minimize(poisson_loss(X, y, beta1) +
+                                           fusedlasso_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        elif self.family == 'normal':
+            prob1 = cp.Problem(cp.Minimize(l2_loss(X, y, beta1) / n) +
+                                           fusedlasso_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        else:
+            raise ValueError('Exponential family not implemented yet')
         prob1.solve()
         self.beta = beta1.value
         return self
-class SL(estimator):
+
+
+class SmoothLassoEstimator(Estimator):
+    def __init__(self, l1=0, l2=0, D=None, family='binomial'):
+                Estimator.__init__(self, l1=l1, l2=l2, D=D,
+                                         family=family)
     def fit(self, X, y):
         n, p = X.shape
-
         beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_f2(X, y, beta1) + SL_pen(beta1, self.l1, self.l2, self.D)))
+        if self.family == 'binomial':
+            prob1 = cp.Problem(cp.Minimize(logit_loss(X, y, beta1) +
+                                           smoothlasso_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        elif self.family == 'poisson':
+            prob1 = cp.Problem(cp.Minimize(poisson_loss(X, y, beta1) +
+                                           smoothlasso_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        elif self.family == 'normal':
+            prob1 = cp.Problem(cp.Minimize(l2_loss(X, y, beta1) / n) +
+                                           smoothlasso_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        else:
+            raise ValueError('Exponential family not implemented yet')
         prob1.solve()
         self.beta = beta1.value
         return self
-class FL(estimator):
+
+
+
+class GenElasticNetEstimator(Estimator):
+    def __init__(self, l1=0, l2=0, D=None, family='binomial'):
+                Estimator.__init__(self, l1=l1, l2=l2, D=D,
+                                         family=family)
     def fit(self, X, y):
         n, p = X.shape
-
         beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_f2(X, y, beta1) + FL_pen(beta1, self.l1, self.l2, self.D)))
+        if self.family == 'binomial':
+            prob1 = cp.Problem(cp.Minimize(logit_loss(X, y, beta1) +
+                                           ee_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        elif self.family == 'poisson':
+            prob1 = cp.Problem(cp.Minimize(poisson_loss(X, y, beta1) +
+                                           ee_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        elif self.family == 'normal':
+            prob1 = cp.Problem(cp.Minimize(l2_loss(X, y, beta1) / n) +
+                                           ee_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        else:
+            raise ValueError('Exponential family not implemented yet')
         prob1.solve()
         self.beta = beta1.value
         return self
-class EN(estimator):
+
+
+
+class ElasticNetEstimator(Estimator):
+    def __init__(self, l1=0, l2=0, D=None, ):
+                Estimator.__init__(self, l1=l1, l2=l2, D=None,
+                                         family='binomial')
     def fit(self, X, y):
         n, p = X.shape
-
-
         beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_f2(X, y, beta1) + EN_pen(beta1, self.l1, self.l2)))
+        if self.family == 'binomial':
+            prob1 = cp.Problem(cp.Minimize(logit_loss(X, y, beta1) +
+                                           elasticnet_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        elif self.family == 'poisson':
+            prob1 = cp.Problem(cp.Minimize(poisson_loss(X, y, beta1) +
+                                           elasticnet_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        elif self.family == 'normal':
+            prob1 = cp.Problem(cp.Minimize(l2_loss(X, y, beta1) / n) +
+                                           elasticnet_penalty(beta1, self.l1,
+                                           self.l2, self.D)))
+        else:
+            raise ValueError('Exponential family not implemented yet')
         prob1.solve()
         self.beta = beta1.value
         return self
-class OUR(estimator):
-    def fit(self, X, y):
-        n, p = X.shape
 
-        beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(loss_f2(X, y, beta1) + EE_pen(beta1, self.l1, self.l2, self.D)))
-        prob1.solve(solver = cp.ECOS, abstol = 1e-4)
-        self.beta = beta1.value
-        return self
+
 class OUR_ADMM(estimator):
     def fit(self, X, y):
         self.beta = Gauss_ADMM(X, y, self.D, self.l1, self.l2)
@@ -229,19 +238,14 @@ class OUR_IP(estimator):
         self.beta = IP(X,y, self.D, self.l1, self.l2)
         return self
 
-def scorer(est, X, y): # default: greater the better
-    return -cp.norm2(y - X @ est.beta).value**2 /X.shape[0]
-
-
-
-
 class GTV(BaseEstimator):
     def __init__(self, l1:float = 0, l2:float = 0,
-                l3 :float= 0, D = None):
+                l3 :float= 0, D = None, family:str = "normal"):
         self.l1 = l1
         self.l2 = l2
         self.l3 = l3
         self.D = D
+        self.family = family
 
     def predict(self, X):
         check_is_fitted(self, "beta")
@@ -258,8 +262,21 @@ class GTV(BaseEstimator):
     def fit(self, X, y):
         n, p = X.shape
         beta1 = cp.Variable(p)
-        prob1 = cp.Problem(cp.Minimize(l2_loss(X, y, beta1) +
-                           GTV_penalty(beta1, self.l1, self.l2, self.l3, self.D)))
+        if self.family == 'binomial':
+            prob1 = cp.Problem(cp.Minimize(logit_loss(X, y, beta1) +
+                                           gtv_penalty(beta1, self.l1,
+                                           self.l2, self.l3,
+                                        self.D)))
+        elif self.family == 'poisson':
+            prob1 = cp.Problem(cp.Minimize(poisson_loss(X, y, beta1) +
+                                           gtv_penalty(beta1, self.l1, self.l2,
+                                           self.l3, self.D)))
+        elif self.family == 'normal':
+            prob1 = cp.Problem(cp.Minimize(l2_loss(X, y, beta1) / n) +
+                                           gtv_penalty(beta1, self.l1, self.l2,
+                                           self.l3, self.D)))
+        else:
+            raise ValueError('Exponential family not implemented yet')
         prob1.solve()
         self.beta = beta1.value
         return self
