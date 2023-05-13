99978db695810278d3fe3e7b6782b221fe1ed78c
vvr <vvr@vvrs-MacBook-Pro.local>
2023-05-13 12:32:05 -0500

    added requirements file, new parallel greedy cgd method function, removed and made separate function for primal-dual preprocessing from cgd methods, added cgd_main, which is to test and run coordinate descent functions

---
+++ src/cgd_main.py
@@ -0,0 +1,88 @@
+from concurrent.futures import process
+from tracemalloc import start
+from turtle import st
+from unittest import result
+from multiprocess import Process, Queue
+import matplotlib.pyplot as plt
+from simulations.examples import SmoothStair, BarbellGraph, GeneralGraph
+import numpy as np
+import pywt
+from numpy import linalg as la
+import timeit
+from copy import deepcopy
+from simulations.covariances import toeplitz_covariance
+from simulations.sample_data import gaussian_sample
+import networkx as nx
+import numpy as np
+from multiprocessing import shared_memory, Process, Lock
+from multiprocessing import cpu_count, current_process
+from multiprocessing import Process, Value, Array
+import timeit
+from src.cgd_solver import cgd_solver, primal_dual_preprocessing, cgd_greedy_parallel
+
+import time
+
+
+import sys
+import os
+
+os.getcwd()
+
+from src.parallel_fns import print_func, print_func2, test_fn, compute_update, add_one, f, compute_and_update
+#print_func("America")
+
+if __name__ == "__main__":  # confirms that the code is under main function
+    #stairs = SmoothStair(n_repeat = 150)
+    #X, y = gaussian_sample(5000, stairs.n_nodes, beta_star = stairs.beta_star, Psi = stairs.Psi, sigma = 1)
+    n = 200
+    p = 0.8
+    m1 = 30
+    m2 = 6
+    m = 120
+    sizes = [55, 78, 88]
+    probs = [[0.25, 0.05, 0.02], [0.05, 0.35, 0.07], [0.02, 0.07, 0.40]]
+    G = nx.powerlaw_cluster_graph(n, m, p)
+
+    barbell = GeneralGraph(G, 60, 70)
+
+    X, y = gaussian_sample(5000, barbell.n_nodes, beta_star = barbell.beta_star, Psi = np.eye(barbell.n_nodes), sigma = 2)
+
+#test.n_nodes
+
+#plt.plot(test.beta_star)
+
+    #barbell = BarbellGraph(length_chain = 50, size_clique = 50)
+    #X, y = gaussian_sample(10000, barbell.n_nodes, beta_star = barbell.beta_star, Psi = np.eye(barbell.n_nodes), sigma = 2)
+
+    dual_params = primal_dual_preprocessing(X, y, barbell.incidence, lambda2 = 1)
+
+    start_time = timeit.default_timer()
+    beta = cgd_greedy_parallel(dual_params, lambda1 = 5, eps = 1e-4, max_it = 50000)
+    end_time = timeit.default_timer()
+
+    start_time2 = timeit.default_timer()
+    beta_normal = cgd_solver(dual_params, lambda1 = 5)
+    end_time2 = timeit.default_timer()
+
+
+    #better tracking of process starting and finishing 
+
+    #print("time_elapsed: ", end_time - start_time)
+    #print(update_counter.value)
+
+    print('time_elapsed_normal:' , end_time2 - start_time2)
+    print('time_elapsed_greedy:', end_time - start_time)
+    print("normed diff normal", la.norm(beta_normal - barbell.beta_star)/np.sqrt(len(beta_normal)))
+    print("normed diff greedy", la.norm(beta - barbell.beta_star)/np.sqrt(len(beta)))
+
+
+
+    fig, ax = plt.subplots()
+    ax.plot(beta, label = "Greedy")
+    ax.plot(beta_normal, label = "Ordinary")
+    ax.plot(barbell.beta_star, label = "True")
+    plt.legend()
+    plt.show()
+
+    
+    #print(X)
--- src/cgd_solver.py
+++ src/cgd_solver.py
@@ -3,19 +3,28 @@
 import numpy as np
 import pywt
 from numpy import linalg as la
+from multiprocessing import shared_memory, Process, Lock
+from multiprocessing import cpu_count, current_process
+from multiprocessing import Process, Value, Array
+from src.parallel_fns import print_func, print_func2, test_fn, compute_update, add_one, f, compute_and_update
 
-
-
-def cgd_solver(X, y, Gamma, lambda1, lambda2, eps = 1e-4, max_it = 50000):
+def primal_dual_preprocessing(X, y, Gamma, lambda2):
     m, p = Gamma.shape
     X_til, y_til = np.vstack((X, np.sqrt(2*lambda2) * Gamma)), np.concatenate((y, np.zeros(m)))
     X_til_pinv = la.pinv(X_til)
 
     y_v = X_til @ X_til_pinv @ y_til
     Gamma_v = Gamma @ X_til_pinv
-
+    
     Q = Gamma_v @ Gamma_v.T
     b = Gamma_v @ y_v
+
+    return (m, X_til_pinv, Q, b, y_v, Gamma_v)
+
+
+def cgd_solver(preprocessed_params, lambda1, eps = 1e-4, max_it = 50000):
+    
+    m, X_til_pinv, Q, b, y_v, Gamma_v = preprocessed_params
 
     u = np.zeros(m)
     n_iter = 0
@@ -42,6 +51,42 @@ def cgd_solver(X, y, Gamma, lambda1, lambda2, eps = 1e-4, max_it = 50000):
     beta = X_til_pinv @ (y_v - Gamma_v.T @ u)
     return beta
 
+
+def cgd_greedy_parallel(preprocessed_params, lambda1, eps = 1e-4, max_it = 50000): 
+
+    m, X_til_pinv, Q, b, y_v, Gamma_v = preprocessed_params
+
+    n_iter = 0
+    update_loops = 0
+    prev_u = 0 # For stopping criteria
+    max_it = 50000
+    processors = 5 #nprocs #be careful of how many processors to use
+    eps = 1e-5
+    update_counter = Value('i', 0)
+    u_arr = Array('f', np.zeros(m))
+    grad_arr = Array('f', -np.copy(b))
+
+    n_iter +=1
+    print(n_iter)
+    if n_iter >= max_it:
+        #raise ValueError("Iterations exceed max_it")
+        print("Iterations exceed max_it")
+        #return (X_til_pinv @ (y_v - Gamma_v.T @ u)), n_iter, update_loops
+
+    procs = []
+
+    split, mod = divmod(m, processors)
+
+    for i in range(processors): 
+        p = Process(target=compute_and_update, args=(u_arr, grad_arr, update_counter, Q, eps, lambda1, i*split+min(i, mod), (i+1)*split+min(i+1, mod)))
+        procs.append(p)
+        p.start()
+
+    for proc in procs:
+        proc.join()
+
+    beta = X_til_pinv @ (y_v - Gamma_v.T @ np.array(u_arr[:]))
+    return beta
 
 # ### Generate stair shape toy example
 
